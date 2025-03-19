4151bc221c767819c9afefb93dcaff6ffd8a7eaa
yalfmon0307 <montesyalfred0@gmail.com>
2025-03-19 10:14:15 -0600

    creando los script

--- backend/db.js
+++ backend/db.js
@@ -16,4 +16,14 @@ if (query) {
     console.log("Connected to database");
 }
 
+const query1 = await pool.query("CREATE TABLE IF NOT EXISTS territories (territory_id UUID NOT NULL DEFAULT uuid_generate_v4(),territory_number INTEGER NOT NULL,territory_names CHARACTER VARYING(36) NOT NULL,PRIMARY KEY (territory_id),UNIQUE (territory_number));");
+
+const query2 = await pool.query("CREATE TABLE IF NOT EXISTS home (home_id UUID NOT NULL DEFAULT uuid_generate_v4(),name CHARACTER VARYING(100) NOT NULL,age INTEGER NOT NULL,family CHARACTER VARYING(100) NOT NULL,adress CHARACTER VARYING(200) NOT NULL,territory_number INTEGER NOT NULL,PRIMARY KEY (home_id),FOREIGN KEY (territory_number) REFERENCES territories(territory_number));");
+
+const query3 = await pool.query("CREATE TABLE IF NOT EXISTS users (user_id UUID NOT NULL DEFAULT uuid_generate_v4(),user_name CHARACTER VARYING(36) NOT NULL,user_email CHARACTER VARYING(36) NOT NULL,user_password CHARACTER VARYING(35) NOT NULL,PRIMARY KEY (user_id),UNIQUE (user_email));");
+
+if (query1 && query2 && query3) {
+    console.log("Tables created successfully");
+}
+
 export default pool;
